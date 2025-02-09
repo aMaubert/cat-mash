ac850b419c95bea5b172816ba11187e0e849b3ac
Allan <allan@MacBook-Pro-de-Allan.local>
2025-02-09 22:32:05 +0100

    update mash view with some css

--- front/src/cat/views/mach.vue
+++ front/src/cat/views/mach.vue
@@ -14,35 +14,45 @@ const secondCat =  computed<Cat>(() => props.secondCat as Cat);
 <template>
   <div class="page">
     <div class="left">
-      <img :src="firstCat.imageUrl"
-           class="logo"
-           alt="logo">
+      <button class="frame">
+        <b class="like">
+          J'aime
+        </b>
+        <img
+             :src="firstCat.imageUrl"
+             alt="firstCat" />
+      </button>
     </div>
     <div class="vl">
       <img src="../../assets/logo-removebg.png"
            class="logo"
            alt="logo">
-      <div class="score__container">
-        <div>
-          <svg xmlns="http://www.w3.org/2000/svg"
-               width="24"
-               height="24"
-               viewBox="0 0 24 24">
-            <path fill="#fa6950" d="m11 8.8l-2.9 2.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.6-4.6q.3-.3.7-.3t.7.3l4.6 4.6q.275.275.275.7t-.275.7t-.7.275t-.7-.275L13 8.8V17q0 .425-.288.713T12 18t-.712-.288T11 17z"/>
-          </svg>
-        </div>
-        <div>
-          Classement des chats
-        </div>
-        <div class="user__score">
-          0 match joué
-        </div>
+    </div>
+    <div class="score__container">
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg"
+             width="24"
+             height="24"
+             viewBox="0 0 24 24">
+          <path fill="#fa6950" d="m11 8.8l-2.9 2.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.6-4.6q.3-.3.7-.3t.7.3l4.6 4.6q.275.275.275.7t-.275.7t-.7.275t-.7-.275L13 8.8V17q0 .425-.288.713T12 18t-.712-.288T11 17z"/>
+        </svg>
+      </div>
+      <div>
+        Classement des chats
+      </div>
+      <div class="user__score">
+        0 match joué
       </div>
     </div>
     <div class="right">
-      <img :src="secondCat.imageUrl"
-           class="logo"
-           alt="logo">
+      <button class="frame">
+        <b class="like">
+          J'aime
+        </b>
+        <img
+            :src="secondCat.imageUrl"
+            alt="secondCat" />
+      </button>
     </div>
   </div>
 </template>
@@ -61,23 +71,27 @@ const secondCat =  computed<Cat>(() => props.secondCat as Cat);
 .right, .left {
   width: 49%;
   color: #42b883;
-  align-items: center;
+  display: flex;
 }
 .logo {
   position: relative;
   left: 50%;
-  width: 10em;
+  width: 15vh;
   transform: translate(-50%);
 }
 .score__container {
   background-color: #feffed;
   color: #fa6950;
-  position: relative;
+
+  position: absolute;
+  bottom: 0;
+  left: 35%;
+
+  width: 30%;
+  height: 10%;
+
   text-align: center;
-  top: 69.5%;
-  transform: translate(-47%);
-  width: 30vw;
-  height: 10vh;
+
   border-top-left-radius: 1em;
   border-top-right-radius: 1em;
 
@@ -86,8 +100,55 @@ const secondCat =  computed<Cat>(() => props.secondCat as Cat);
   font-size: 0.8em;
 }
 
-.left img, .right img {
-  width: 10em;
+.left .frame, .right .frame {
+  background-color: #feffed;
+
+  width: 80%;
+  height: 70%;
+  margin: 20% 10%;
+  border-radius: 12%;
+  border: 0;
+  padding: 0;
+
+  transition: .5s ease;
+
+  cursor: pointer;
+}
+
+.left .frame img, .right .frame img {
+  transition: .5s ease;
+  opacity: 1;
+  width: 100%;
+  height: 100%;
+  border-radius: 12%;
+}
+
+.frame .like {
+  color: #fa6950;
+
+  opacity: 0;
+
+  font-size: 3em;
+}
+
+.left .like {
+  position: absolute;
+  top: 70%;
+  left: 19%;
+}
+
+.right .like {
+  position: absolute;
+  top: 70%;
+  right: 19%;
+}
+
+.frame:hover img {
+  opacity: 0.2;
+}
+
+.frame:hover .like {
+  opacity: 1;
 }
 
 
